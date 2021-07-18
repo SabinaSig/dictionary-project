a328a971117e4e4252e8a0764e1fb077ba789972
SabinaSig <77678314+SabinaSig@users.noreply.github.com>
2021-07-18 23:45:14 +0200

    Added a listren button to the project

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Phonetic(props) {
-    return "Hello";
+    return (
+        <div className="Phonetic">
+            <a href={props.phonetic.audio} target="_blank">
+                Listen
+            </a>
+            <br />
+            {props.phonetic.text}
+        </div>
+    );
 } 
